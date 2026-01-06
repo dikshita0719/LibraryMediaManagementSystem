579bb69c6c3fa77a71d5a255a478e7ff6107b2f9
dikshita0719 <dikshitabhattarai19@gmail.com>
2026-01-06 20:40:22 +0100

    Basic program that prints the given information, like title, and archives an item when instructed.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
+#[derive(Debug)] //REQUIRED for printing
 enum MediaType{
     Book,
     Disc,
     Movie
 }
 
+#[derive(Debug)]
 enum Status{
     Available,
     NotInStock,
-    Borrowed
+    Borrowed,
+    Archived
 }
 
+#[derive(Debug)]
 struct LibraryItem{
     id: u32,
     name: String,
     media: MediaType,
-    status: Status
+    status: Status,
+    shelf_location: ShelfLocation
 }
 
+#[derive(Debug)]
 struct ShelfLocation(String, u8,u8); //String is the area(Like K for Kunst, u8-shelfNumber, u8-Rownumber
 
 struct DatabaseConnection;  // UnitLikeStruct
@@ -29,6 +35,40 @@ impl DatabaseConnection {
     }
 }
 
+impl LibraryItem{
+    fn new(id: u32, name: String, media: MediaType, status: Status, shelf_location: ShelfLocation)->Self{
+        return Self{
+            id,
+            name,
+            media,
+            status,
+            shelf_location
+        };
+    }
+
+    fn get_title(&self)->String{
+        return self.name.clone(); //does a deep copy of the String without actually losing the ownership (unlike move for primitive Datatypes)
+    }
+    fn set_title(&mut self, new_title: String){
+        self.name = new_title;
+    }
+    fn archive (mut self){
+        self.status = Status::Archived;
+        println!("Archived {:?}: {} \n.Media no longer available.", self.media, self.name);
+    }
+}
+fn print_item_info(item: &LibraryItem){ //immutable borrwing
+    println!("{:#?}", item);
+}
 fn main() {
-    println!("Hello, world!");
+    let first = LibraryItem::new(1,String::from("Da Vinci Code"), MediaType::Book, Status::Available,ShelfLocation(String::from("W02"), 2,6)) ;
+    let second = LibraryItem::new(3,String::from("Pink Floyd: Dark Side of the Moon"), MediaType::Disc, Status::Borrowed,ShelfLocation(String::from("K02"), 5,9)) ;
+
+    println!("{:?}", first.get_title());
+    println!("{:?}", second.get_title());
+    second.archive();
+    //println!("{:?}", second.get_title()); //Error because the value has been moved in the previous line
+
+    print_item_info(&first);
+
 }
